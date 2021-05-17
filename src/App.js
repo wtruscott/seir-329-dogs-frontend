7bb631b86904fe89113a0bb6f42c28cf9c1d3c9b
William Truscott <williamtruscott@Williams-MacBook-Pro.local>
2021-05-17 16:23:36 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -5,25 +5,118 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+
+  // URL in a variable
+  const url = "https://dogs-backend-329-wt.herokuapp.com"
+  // State to hold the list of dogs
+  const [dogs, setDogs] = React.useState([])
+
+    // Empty Dog - For the Create Form
+    const emptyDog = {
+      name: "",
+      age:0,
+      img: ""
+    }
+
+    const [selectedDog, setSelectedDog] = React.useState(emptyDog)
+  // Function to get list of Dogs
+  const getDogs = () => {
+  // make a get a request to this url
+  fetch(url + "/dog/")
+  // use .then to take action when the response comes in
+  // convert data into js object
+  .then((response) => response.json())
+  // use the data from the response
+  .then((data) => {
+    setDogs(data)
+  })
+  }
+  // useEffect, to get the data right away
+  React.useEffect(() => {
+    getDogs()
+  }, [])
+
+ //handleCreate - function for when the create form is submitted
+ const handleCreate = (newDog) => {
+  fetch(url + "/dog/", {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(newDog)
+  })
+  .then(() => getDogs())
+}
+
+  // handleUpdate - function for when the edit form is submitted
+  const handleUpdate = (dog) => {
+    fetch(url + "/dog/" + dog._id, {
+      method: "PUT",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(dog)
+    })
+    .then(() => getDogs())
+  }
+
+  // function to specify which dog we are updated
+const selectDog = (dog) => {
+  setSelectedDog(dog)
+}
+
+// deleteDog to delete individual dog
+const deleteDog = (dog) => {
+  fetch(url + "/dog/" + dog._id, {
+    method: "delete"
+  })
+  .then(() => {
+    getDogs()
+  })
+}
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to="/create">
+        <button>Add Dog</button>
+      </Link>
       <main>
-        <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+      <Switch>
+      <Route
+            exact
+            path="/"
+            render={(rp) => (
+              <Display 
+              {...rp} 
+              dogs={dogs} 
+              selectDog={selectDog}
+              deleteDog={deleteDog} 
+              />
+            )}
+          />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form
+                {...rp}
+                label="create"
+                dog={emptyDog}
+                handleSubmit={handleCreate}
+              />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form 
+              {...rp} 
+              label="update" 
+              dog={selectedDog} 
+              handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  // destruct the dogs from props
+  const {dogs} = props
+
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+        <article key={dog._id}>
+        <img src={dog.img}/>
+        <h1>{dog.name}</h1>
+        <h3>{dog.age}</h3>
+        <button onClick={() => {
+            props.selectDog(dog)
+            props.history.push("/edit")
+          }}>
+            edit
+          </button>
+          <button onClick={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>
+      </article>
+      ))}
+    </div>
+  )
+
+  const loading = () => <h1>Loading</h1>
+  
+  return dogs.length > 0 ? loaded() : loading ()
 };
 
 export default Display;
